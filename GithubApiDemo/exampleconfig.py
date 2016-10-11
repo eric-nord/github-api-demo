249aa9970acc109397227987a6c441f22b86bd22
eric.w.nord <eric.w.nord@gmail.com>
2016-10-11 15:57:20 -0400

    update org_urls

--- GithubApiDemo/exampleconfig.py
+++ GithubApiDemo/exampleconfig.py
@@ -1,11 +1,13 @@
+#Required for access to the Github API
 username ="githubname@email.com"
 password ="githubpassword"
-#Might need to include this in a secure way for the app
-#Currently set to S3 full access security group
+
+#Set up S3 user and add to S3 full access security group
 AWS_ACCESS_KEY_ID = 'Amazon goo'
 AWS_SECRET_ACCESS_KEY = 'Amazon secret goo'
 
 #gmail smpt info also may need to be embedded securely...
-#or setup send only SMTP via  on the host server... 
+#May require enabling less secure applications https://www.google.com/settings/security/lesssecureapps
+#May require diabling Capthca. https://accounts.google.com/b/0/DisplayUnlockCaptcha 
 gmailuser = 'username@gmail.com'
 gmailpass = 'gmailpassword'
--- GithubApiDemo/org_urls.py
+++ GithubApiDemo/org_urls.py
@@ -1,11 +1,21 @@
+"""
+NAme
+does this
+"""
+__version__ = "0.0.1"
+__author__ = "Eric Nord"
+__all__ [,,,] # for import *
+
 import json
 import config
 import requests
 from requests.auth import HTTPBasicAuth
 
-#Provides list of organizations the user is associated with
-#Includes private and public members
-def getOrgs():
+def get_orgs():
+  """
+  Provides list of organizations the user is associated with
+  Includes private and public members
+  """
   orgUrls = []
   orgs = requests.get('https://api.github.com/user/orgs', 
                       auth=HTTPBasicAuth(config.username, config.password))
